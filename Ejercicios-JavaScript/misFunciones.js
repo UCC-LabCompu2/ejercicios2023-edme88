2471509acd1ffe8fd131c2d30a680f1f1362bd8c
edme88 <edmealiciardi@gmail.com>
2023-05-30 11:41:08 -0300

    Ejercicios canvas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -99,6 +99,10 @@ let verLetra = (id, value) => {
     }
 }
 
+/**
+ * Genera la URL
+ * @method generarUrl
+ */
 let generarUrl = () => {
     const dist = document.getElementById("distancia").value;
     const uni = document.getElementsByName("unidades")[0].value;
@@ -107,6 +111,10 @@ let generarUrl = () => {
     window.open(urlCompl);
 }
 
+/**
+ * Genera la URL
+ * @method generarUrl
+ */
 let cargarValor = () => {
     let urlCompleta = window.location.href;
     console.log(urlCompleta);
@@ -116,6 +124,7 @@ let cargarValor = () => {
     const unidad = urlCompleta[2];
     document.getElementById("dist").value = `${distancia} ${unidad}`;
 }
+
 
 let guardarLS = () => {
     let dist = document.getElementById("distancia").value;
@@ -210,4 +219,110 @@ let cargarListeners = () => {
 
 let cargarListenersEjemplo = () => {
     document.getElementById("myCanvas").addEventListener("mousemove", dibujar);
+}
+
+let dibujarCuadriculado = () => {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    const anchoMax = canvas.width;
+    const alturaMax = canvas.height;
+    const paso = 20;
+    let ejeX = -24; //Calcular este numero
+    let ejeY = -14; //Calcular este numero
+
+    //Líneas Verticales
+    for(let i=paso;i<anchoMax;i+=paso){
+        ctx.beginPath();
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, alturaMax);
+        ctx.strokeStyle = "#797575";
+        ctx.stroke();
+        ctx.font="10px Arial";
+        ctx.fillStyle = "#333";
+        ctx.fillText(ejeX, i, alturaMax/2);
+        ctx.closePath();
+        ejeX++;
+    }
+
+    //Líneas Horizontales
+    for(let i=paso;i<alturaMax;i+=paso){
+        ctx.beginPath();
+        ctx.moveTo(0, i);
+        ctx.lineTo(anchoMax, i);
+        ctx.strokeStyle = "#797575";
+        ctx.stroke();
+        ctx.font="10px Arial";
+        ctx.fillStyle = "#333";
+        ctx.fillText( ejeY, anchoMax/2, i);
+        ctx.closePath();
+        ejeY++;
+    }
+
+    //Eje X
+    ctx.beginPath();
+    ctx.moveTo(0, alturaMax/2);
+    ctx.lineTo(anchoMax, alturaMax/2);
+    ctx.strokeStyle = "#b20707";
+    ctx.stroke();
+    ctx.closePath();
+
+    //Eje Y
+    ctx.beginPath();
+    ctx.moveTo(anchoMax/2, 0);
+    ctx.lineTo(anchoMax/2, alturaMax);
+    ctx.strokeStyle = "#b20707";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+var x = 0;
+var dx = 2;
+let animarAuto = () => {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    let img = new Image();
+    img.src = "images/auto.png";
+
+    img.onload = function (){
+        canvas.width = canvas.width;
+        ctx.drawImage(img, x, 100);
+    }
+    x += dx;
+    console.log("La coordenada X es: "+x);
+    if(x>canvas.width){
+        x = 0;
+    }
+}
+
+let dibujarImagen = (posX, posY) => {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+    const anchoMax = canvas.width;
+    const alturaMax = canvas.height;
+
+    canvas.width = canvas.width;
+
+    console.log(posX, posY);
+    let img = new Image();
+    img.src = "images/auto.png";
+
+    if(posX<0 || posY<0 || posX>=anchoMax || posY>=alturaMax){
+        openDialog();
+    }else{
+        img.onload = function (){
+            ctx.drawImage(img, posX, posY);
+        }
+    }
+}
+
+let cerrarDialog = () => {
+    const dialog = document.getElementById("myDialog");
+    dialog.close();
+}
+
+let openDialog = () => {
+    const dialog = document.getElementById("myDialog");
+    dialog.showModal();
 }
